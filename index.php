23a897e7b264b56c3419337a2b5e8c310d9a01d1
abdobenh <96568696+abdobenh@users.noreply.github.com>
2021-12-23 09:31:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1> boiuchbdhgirhgi urhguirhgeuihgui</h1>
+  </body>
+</html>
 <?php
 $msg = "First line of text\nSecond line of text";
 $msg = wordwrap($msg,70);
